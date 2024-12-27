5a99a35488463a29d934870ca4aadb33966ec8de
Anderson Gabriel <gabrielvalencasa98@gmail.com>
2024-12-27 08:15:38 -0300

    Add Popover to the language switch for now

--- src/components/ui/language-switch.tsx
+++ src/components/ui/language-switch.tsx
@@ -7,23 +7,24 @@ import {
   TooltipProvider,
   TooltipTrigger,
 } from "@/components/ui/tooltip";
+import { Popover, PopoverContent, PopoverTrigger } from "@/components/ui/popover";
 
 export default function LanguageSwitch() {
   return (
     <>
       <TooltipProvider>
-        <Tooltip>
-          <TooltipTrigger asChild>
-            <div className="flex items-center space-x-2 text-white p-4 rounded-lg">
-              <Label className="text-sm text-muted-foreground">PT-BR</Label>
+          <Popover>
+            <PopoverTrigger asChild>
+              <div className="flex items-center space-x-2 text-white p-4 rounded-lg">
+                <Label className="text-sm text-muted-foreground">PT-BR</Label>
               <Switch className="data-[state=checked]:bg-primary-light data-[state=unchecked]:bg-primary-dark border-primary-dark/60" />
               <Label className="text-sm font-medium">ENG</Label>
             </div>
-          </TooltipTrigger>
-          <TooltipContent className="bg-primary-dark text-md p-4 rounded-lg text-white">
+          </PopoverTrigger>
+          <PopoverContent className="bg-primary-dark text-center text-md p-4 rounded-lg text-white">
             <p>Still in development {":)"}</p>
-          </TooltipContent>
-        </Tooltip>
+          </PopoverContent>
+        </Popover>
       </TooltipProvider>
     </>
   );
