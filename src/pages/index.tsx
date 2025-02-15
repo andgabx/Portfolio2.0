8ad1f5a861592125218bb0f301deb085451dea80
Anderson Gabriel <gabrielvalencasa98@gmail.com>
2025-02-15 10:31:04 -0300

    feat: Add metadata and title to home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,7 @@
 import { Inter } from "next/font/google";
 import { HomePage } from "@/components";
 import { useTranslations } from "next-intl";
+import Head from "next/head";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -8,8 +9,14 @@ export default function Home() {
   const t = useTranslations("common");
 
   return (
-    <main className={inter.className}>
-      <HomePage />
-    </main>
+    <>
+      <Head>
+        <title>I'm Anderson!</title>
+        <meta name="description" content="My portfolio" />
+      </Head>
+      <main className={inter.className}>
+        <HomePage />
+      </main>
+    </>
   );
 }
